e9706ba95d9cfcb6aa5036a4fffa2086bdf0b2d9
yongbum <zlzlzl002@naver.com>
2018-02-19 18:23:11 +0900

    tuple/Dict/List/function DataTypeTest / FunctionTest 정리함

---
+++ Hello/test/DataTypeTest.py
@@ -0,0 +1,67 @@
+#-*- coding:utf-8 -*-
+
+# Dict 
+
+mem1= {"num":1, "name":"kimgura", "addr":"zoo"}
+
+# 특정방 참조하기
+a=mem1["num"]
+print a
+
+# num 수정 해보기
+mem1["num"]=999
+
+# num 삭제 해보기
+del mem1["num"]
+
+result =mem1.keys()
+
+# key 값 
+for tmp in mem1.keys():
+    print tmp, ":" ,mem1[tmp] 
+
+# tuple  정보 수정 삭제 할수 없다 List type 보다 빠르다
+num=(1,2,3,4,5)
+
+# del num[1] tuple type 이런거 안됨
+
+for tmp in num:
+    print "tmp:", tmp
+
+# 방 1개 짜리 tuple 만들때 ,
+result=("kim",)
+
+# 여러개 한번에 할당하기
+a, b ,c = 10,20,30
+print a,b,c
+
+# 서로 바꾸기
+myName="ansdj"
+yourName="ansdj2"
+
+myName , yourName= yourName, myName
+print "myName:", myName, "yourName:" , yourName
+
+# list type 방번호 생각하시옹
+name=["kim","lee","park"]
+
+# 추가하기
+name.append("Moon")
+
+# 삭제하기
+del name[0]
+
+# 수정하기
+name[0]="Han"
+
+# 오름 차순 정렬 하는법
+nums=[10,20,40,50,30,60]
+nums.sort()
+for tmp in nums :
+    print tmp
+
+# nums 방의 갯수 구하기
+print len(nums)
+
+
+
---
+++ Hello/test/FunctionTest.py
@@ -0,0 +1,55 @@
+#-*- coding:utf-8 -*-
+
+# 함수 생성 하기
+def test1():
+    pass
+
+# 함수 호출하기
+test1()
+
+# 줄을 맞춰줘야 한다  
+def test2():
+    print "gura"
+    print "gura2"
+    print "gura3"
+    
+# arg 인자를 전달하는 함수 생성
+def test3(arg):
+    print "arg:", arg
+
+# test3() 한개 인자라도 전달안하면 안된다. 오류남
+# test3("gogo","gogogo") 1개의 인자만 전달가능 사용하려면 *args 
+test3("gogo")
+
+# 인자를 2개 전달하는 함수 생성
+def test4(a,b):
+    print "a:", a, "b:", b
+
+test4(1,2)
+
+# *args 전달하는 함수 생성
+def test5(*args):
+    # tuple type 이다
+    print "args:", args
+
+test5() # *args 인자를 절단 안해도되고
+test5(1) # *args 인자를 1개만 전달해도 된다 그이상도 가능
+test5('kim','lee')
+
+# **kwargs keyword arguments
+def test6(**kwargs):
+    # Dict type 이다
+    print "kwargs:", kwargs
+    
+test6()
+test6(num=2,name="gura")
+test6(num=3,name="gura2",isMan=True)
+
+# *args **kwargs 같이 사용하기
+def test7(*args, **kwargs):
+    print "args:",args , "kwargs:", kwargs
+
+test7()
+test7(1)
+test7(name="mouse")
+test7(2,name="key")
